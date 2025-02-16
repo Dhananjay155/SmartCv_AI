b0b42250ed668edc53a721d4e45c8697a8d4dfb3
Dhananjay155 <dhananjaychoudhary017@gmail.com>
2025-02-17 00:10:33 +0530

    updated code

---
+++ ai-resume-builder-frontend/src/components/custom/Header.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import logo from "../../assets/Smart.png";
+import { Button } from "../ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "@/Services/login";
+import { addUserData } from "@/features/user/userFeatures";
+
+function Header({ user }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(
+      `Printing From Header User ${user ? "Found" : "Not Found"}`
+    );
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      const response = await logoutUser();
+      if (response.statusCode === 200) {
+        dispatch(addUserData(""));
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div
+      id="printHeader"
+      className="flex justify-between px-10 py-5 shadow-md items-center"
+    >
+      <Link to="/" className="cursor-pointer">
+        <img src={logo} alt="logo" width={100} height={100} />
+      </Link>
+      {user ? (
+        <div className="flex items-center gap-4">
+          <Button
+            variant="outline"
+            onClick={() => navigate("/dashboard")}
+          >
+            Dashboard
+          </Button>
+          <Button onClick={handleLogout}>Logout</Button>
+        </div>
+      ) : (
+        <Link to="/auth/sign-in">
+          <Button>Get Started</Button>
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default Header;
